a5a66538938159dcf464ab8d5f59ed08f2617bb9
Hersh Thakur <hersh.thakur@gmail.com>
2023-10-26 12:17:49 +0100

    doing the the homepage

---
+++ src/components/Nav.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
+
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: sessionData } = useSession();
+
+  if (sessionData) {
+    return (
+      <nav className="absolute left-0 top-0 z-10 flex w-full flex-col items-center justify-between gap-8 bg-slate-950 bg-transparent p-7 backdrop-blur-md md:fixed md:flex-row md:gap-0">
+        <div className="font-poppins flex w-full items-center justify-between text-xl font-bold  tracking-tight text-neutral-100 md:text-4xl">
+          <Link href="/">JRNL</Link>
+          <div className="flex md:hidden" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <XMarkIcon width={30} /> : <Bars3Icon width={30} />}
+          </div>
+        </div>
+        <ul
+          className={`font-poppins flex flex-col text-xl font-medium text-neutral-100 md:flex-row md:items-center md:justify-end md:gap-10 ${
+            !isOpen && "hidden md:flex"
+          }`}
+        >
+          <Link href="/entries">Entries</Link>
+          <Link href="/write">Write</Link>
+          <button className="w-min" onClick={() => void signOut()}>
+            Logout
+          </button>
+        </ul>
+      </nav>
+    );
+  }
+};
+
+export default Nav;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,35 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 
+import { Poppins, Montserrat } from "next/font/google";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import Nav from "@components/Nav";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "400", "800"],
+  variable: "--font-poppins",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300"],
+  variable: "--font-montserrat",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <div className={`${poppins.variable} ${montserrat.variable}`}>
+      <SessionProvider session={session}>
+        <Nav />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </div>
   );
 };
 
